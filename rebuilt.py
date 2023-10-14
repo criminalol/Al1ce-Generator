c2e1bf92263fc7789356ea47c6cf64d35fad106b
VΛSƬ <60797067+imvast@users.noreply.github.com>
2023-10-13 22:15:08 -0500

    Create rebuilt.py

---
+++ rebuilt.py
@@ -0,0 +1,30 @@
+# more soon im on vaca so kinda hard on mobile lmao
+
+import threading
+import json
+import time
+import requests
+import tls_client
+import os
+import random
+import base64
+import websocket
+import re
+import ctypes
+import keyboard
+import traceback
+import sys
+import base64
+import win32security
+import platform
+import subprocess
+import pygame
+from pystyle import *
+from datetime import datetime
+from os import listdir, path
+from random import choice
+from os.path import isfile, join
+from pypresence import Presence
+from colorama import *
+from base64 import b64encode
+ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1'
